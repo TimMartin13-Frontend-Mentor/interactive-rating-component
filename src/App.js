d1994bfd210f9fae430002e5b27699f3378efaa4
Tim Martin <timmartin13@gmail.com>
2022-06-20 16:37:38 -0500

    ThankYouCard mobile layout

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import ThankYouCard from "./components/ThankYouCard";
 
 function App() {
   
-  const [rated, setRated] = useState(false);
+  const [rated, setRated] = useState(true);
 
   return (
     <div className="App">
       <main>
-        { rated ? <ThankYouCard /> : <RatingCard /> }
+        { rated ? <ThankYouCard rating={ "4" }/> : <RatingCard /> }
       </main>
       <footer>
         <Attribution />
@@ -21,27 +21,3 @@ function App() {
 }
 
 export default App;
-
-// <!-- Rating state start -->
-
-//   How did we do?
-
-//   Please let us know how we did with your support request. All feedback is appreciated 
-//   to help us improve our offering!
-
-//   1 2 3 4 5
-
-//   Submit
-
-//   <!-- Rating state end -->
-
-//   <!-- Thank you state start -->
-
-//   You selected <!-- Add rating here --> out of 5
-
-//   Thank you!
-
-//   We appreciate you taking the time to give a rating. If you ever need more support, 
-//   don’t hesitate to get in touch!
-
-//   <!-- Thank you state end -->
--- src/components/ThankYouCard.js
+++ src/components/ThankYouCard.js
@@ -1,10 +1,19 @@
 import React from "react";
 import thankYouCardStyles from "./ThankYouCard.module.css"
 
-function ThankYouCard() {
+function ThankYouCard({ rating }) {
   return (
     <div className={ thankYouCardStyles.card }>
-      This is the Thank you Card
+      <div className={ thankYouCardStyles.cardContainer }>
+        <img 
+          src={ process.env.PUBLIC_URL + "/images/illustration-thank-you.svg" } className={ thankYouCardStyles.icon } 
+          alt="credit card machine"></img>
+        <div className={ thankYouCardStyles.rated }>
+          You selected
+          <span>&nbsp;{ rating }&nbsp;</span>out of 5</div>
+        <h2 className={ thankYouCardStyles.heading }>Thank you!</h2>
+        <p className={ thankYouCardStyles.copy }>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+      </div>
     </div>
   )
 }
